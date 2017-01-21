dbb0d09392b8ddc815b5db6d369dca5cb81d0dc8
Geekolas <geekoval@gmail.com>
2017-01-21 02:43:41 +0100

    [Add] Rayon React

--- Wavy Jump/Assets/Scripts/PlayerController2D.cs
+++ Wavy Jump/Assets/Scripts/PlayerController2D.cs
@@ -5,24 +5,34 @@ using JumpLib;
 public class PlayerController2D : MonoBehaviour {
 
 	public int speed;
+    public float rayonMax;
+    public GameObject rayon;
 
 	private Vector2 _inputs;
 	private Rigidbody2D rb2d;
     private Vector2 target;
+    private Ray ray;
+    private RaycastHit hit;
 
-	void Start(){
+    void Start()
+    {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update () {
-        if (Input.GetButtonDown("Jump"))
-        {
-            target = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //transform.position = Vector2.Lerp(transform.position, target, (float)speed);
-            _inputs = - (target - (Vector2)transform.position);
-            rb2d.velocity = _inputs * speed;
-        }
-        
+	void Update ()
+    {
 
+    }
+    public void TouchOnScreen ()
+    {
+        target = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _inputs = -(target - (Vector2)transform.position);
+        rb2d.velocity = _inputs * speed;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Box Clicked kgvsdbolwhdkvnc!");
+        TouchOnScreen();
     }
 }
